bd187f1c0e61c6153dc03042fcf66cc518faa1c0
liyang <ls3817@nyu.edu>
2020-07-12 18:02:27 -0400

    add

---
+++ batch/batch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=96:00:00
+#SBATCH --mem=8GB
+#SBATCH --job-name=ms_%j
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ls3817@nyu.edu
+#SBATCH --output=slurm_%j.out
+  
+module purge
+
+VIRTUALENV=$SCRATCH
+RUNDIR=$SCRATCH/low_latency_streaming/qoe_bdq
+
+cd $VIRTUALENV
+
+source ./new_torch/bin/activate
+  
+cd $RUNDIR
+python main.py 
